68ceccb0e395d1fc0c3fb0be5dda1105d1c73059
Johne Alves <johne_af@hotmail.com>
2020-11-27 18:42:18 -0300

    adiciona exercicio 3

---
+++ introducao-a-javascript-e-logica-de-programacao-3/exercise/exercise-3.js
@@ -0,0 +1,16 @@
+//3- Agora inverta o lado do triângulo. Por exemplo:
+
+let n = 5
+let form = [];
+
+for(let k = 1; k <= n; k += 1) {
+    for(let index = 0; index < n; index +=1) {
+        if (index >= (n - k)) {
+            form += '*';
+        } else {
+            form += ' ';
+        }
+    }
+    console.log(form);
+    form = [];
+}
