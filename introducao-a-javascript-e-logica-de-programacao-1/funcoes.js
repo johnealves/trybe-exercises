b01b465c373d81d31f413bdc61bba53c7063ed35
Johne Alves <johne_af@hotmail.com>
2020-11-29 10:46:12 -0300

    finaliza arquivo funcoes.js

--- introducao-a-javascript-e-logica-de-programacao-1/funcoes.js
+++ introducao-a-javascript-e-logica-de-programacao-1/funcoes.js
@@ -195,3 +195,67 @@ function lucroSobreVendas(numeroDeVendas, valorCusto, valorVenda, aliquota) {
 lucroSobreVendas(numeroDeVendas, valorCusto, valorVenda, aliquota)
 
 console.log(lucroTotal)
+
+//Salario Liquido
+let salario = 3000.00;
+let salarioAposINSS;
+let faixa1 = 78.38;
+let faixa2 = 94.01;
+let faixa3 = 125.38;
+
+function cauculoDeDeducaoINSS (salario) {
+    if (salario > 6101.06) {
+        INSS = 713.09
+        salarioAposINSS = salario - 713.09;
+    } else if (salario >= 3134.41) {
+        faixaSalario = salario - 3134.41;
+        INSS = faixa1 + faixa2 + faixa3 + (faixaSalario*0.14);
+        salarioAposINSS = salario - INSS;
+    } else if (salario >= 2089.61) {
+        faixaSalario = salario - 2089.61;
+        INSS = faixa1 + faixa2 + (faixaSalario*0.12);
+        salarioAposINSS = salario - INSS;
+    } else if (salario >= 1045.01) {
+        faixaSalario = salario - 1045.01;
+        INSS = faixa1 + (faixaSalario*0.09);
+        salarioAposINSS = salario - INSS;
+    } else if (salario < 1045.01) {
+        INSS = salario * 0.075;
+        salarioAposINSS = salario - INSS;
+    }
+
+
+    return salarioAposINSS
+}
+
+cauculoDeDeducaoINSS(salario)
+
+
+function calculoImpostoDeRenda (salario) {
+    if (salario > 4664.68) {
+        IR = (0.275 * salario) - 869.36;
+        salarioAposImpostos = salario - IR;
+    } else if (salario >= 3751.06) {
+        IR = (0.225 * salario) - 636.13;
+        salarioAposImpostos = salario - IR;
+    } else if (salario >= 2826.66) {
+        IR = (0.15 * salario) - 354.80;
+        salarioAposImpostos = salario - IR;
+    } else if (salario >= 1903.99) {
+        IR = (0.075 * salario) - 142.80;
+        salarioAposImpostos = salario - IR;
+    } else {
+        IR = 0;
+        salarioAposImpostos = salario - IR;
+    }
+
+
+    return salarioAposImpostos
+}
+
+calculoImpostoDeRenda(cauculoDeDeducaoINSS())
+
+console.log(`O valor do INSS foi de R$ ${INSS}`);
+console.log(`O valor após o desconto do INSS é de R$ ${salarioAposINSS}`);
+console.log(`O valor do Imposto de Renda é de R$ ${IR}`);
+console.log(`O valor do salario após os impostos é de R$ ${salarioAposImpostos}`);
