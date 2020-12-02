f03d12da50b0e55731b290f869d220edb626b080
Johne Alves <johne_af@hotmail.com>
2020-12-01 21:21:10 -0300

    correçao

---
+++ introducao-a-javascript-e-logica-de-programacao-4/exercise-5-aternative.js
@@ -0,0 +1,20 @@
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+
+
+
+
+function contaValoresRepetidos(array) {
+  let contNumero = 0
+  for (let index in array) {
+    contNumero = 0;
+    for()
+  }
+
+  return valorQueMaisSeRepete;
+}
+
+let arrayInt = [2, 3, 2, 5, 8, 2, 3];
+contaValoresRepetidos([2, 3, 2, 5, 8, 2, 3])
+console.log(repetition)
+console.log('O valor que mais se repete é ' + valorQueMaisSeRepete)
